f4adb7c54efb2a31b694644a3fef59434d172a80
ymgn <b4183@oic.jp>
2017-04-23 01:15:51 +0900

    add order_listにnameを追加に伴った変更

--- app/controllers/order_lists_controller.rb
+++ app/controllers/order_lists_controller.rb
@@ -69,6 +69,6 @@ class OrderListsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def order_list_params
-      params.require(:order_list).permit(:account_id, :order_token, :sort_num)
+      params.require(:order_list).permit(:account_id, :order_token, :sort_num, :name)
     end
 end
