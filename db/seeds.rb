e40db016480a7cb7ad71132376a617346890977c
ymgn <b4183@oic.jp>
2017-04-23 01:14:21 +0900

    add seederを作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.new
+user.email = "user@example.com"
+user.login_id = "user1"
+user.password = "hogehoge"
+user.password_confirmation = "hogehoge"
+user.save!
+
+site = Site.new
+site.site_id = "sushi"
+site.site_name = "銀のさら"
+site.url = "https://www.ginsara.jp/"
+site.save!
+
+account = Account.new
+account.user_id = 1
+account.site_id = 1
+account.mail = "user1@example.com"
+account.password = "hogehoge"
+account.save!
+
+order_list = OrderList.new
+order_list.account_id = 1
+order_list.order_token = "hogehoge"
+order_list.sort_num = 1
+order_list.name = "まぐろづくし"
+order_list.save!
+
+order = Order.new
+order.order_list_id = 1
+order.item_id = 1
+order.item_num = 5
+order.save!
