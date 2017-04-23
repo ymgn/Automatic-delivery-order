ab59231652a70610d528d8270c10a3198e546954
ymgn <b4183@oic.jp>
2017-04-23 18:56:19 +0900

    update 復号できる必要があるので、できる方式に変更

--- app/models/account.rb
+++ app/models/account.rb
@@ -2,5 +2,32 @@ class Account < ApplicationRecord
   has_many :order_list
   belongs_to :user
   belongs_to :site
-  has_secure_password
+
+  before_save :encrypt_password
+  
+  # いずれ適切な位置に移動させる
+  SECURE = '6ZEMlruVq1u20bBab6LkFznJfTNgTIqOWUo7lv6zs2uavtKX1u'
+  CIPHER = 'aes-256-cbc'
+
+
+  def encrypt_password
+    self.password = encrypt(self.password)
+  end
+
+  def decrypt_password
+    password = decrypt(self.password)
+  end
+
+
+  # 暗号化
+  def encrypt(password)
+    crypt = ActiveSupport::MessageEncryptor.new(SECURE, CIPHER)
+    crypt.encrypt_and_sign(password)
+  end
+
+  # 復号化
+  def decrypt(password)
+    crypt = ActiveSupport::MessageEncryptor.new(SECURE, CIPHER)
+    crypt.decrypt_and_verify(password)
+  end
 end
