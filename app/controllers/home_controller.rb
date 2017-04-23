28258df81891c8f96bf698454f22a895ada98192
ymgn <b4183@oic.jp>
2017-04-23 16:38:48 +0900

    add ログイン時のホームにアカウントリストがでるように

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,13 @@
 class HomeController < ApplicationController
   def index
+    if user_signed_in?
+      @account_data = {}
+      current_user.account.each do |a|
+        @account_data[a.site.site_name] = []  
+      end
+      current_user.account.each do |a|
+        @account_data[a.site.site_name].append(a)  
+      end
+    end
   end
 end
